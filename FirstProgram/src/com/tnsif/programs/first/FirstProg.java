736051313abb91df953c701f75e5f84db6d44678
Lenovo <Lenovo@DESKTOP-3GVLEGL>
2024-06-22 13:04:17 +0530

    Initial Commit

---
+++ FirstProgram/src/com/tnsif/programs/first/FirstProg.java
@@ -0,0 +1,13 @@
+//This is the first project of core java development
+package com.tnsif.programs.first;
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Falak...!!! Welcome to the world of Programming!!!");
+
+	}
+
+}
