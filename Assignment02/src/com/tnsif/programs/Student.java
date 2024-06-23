a219bd26c9f5ebdcd48fb1ff2cde700dce6d9c6e
Lenovo <Lenovo@DESKTOP-3GVLEGL>
2024-06-23 20:49:29 +0530

    day2 assignment

---
+++ Assignment02/src/com/tnsif/programs/Student.java
@@ -0,0 +1,12 @@
+/*Creating the default constructor for Student */
+
+package com.tnsif.programs;
+
+public class Student {
+	
+	public Student() {
+		
+		System.out.println("Student object is created");
+	}
+
+}
