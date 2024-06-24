bc88ebe58b4143ac78104793913a12796cac833b
Lenovo <Lenovo@DESKTOP-3GVLEGL>
2024-06-24 13:45:29 +0530

    Day 3,4 commit

---
+++ Day3&4_Prog/src/com/tnsif/programs/daythree/SingleInheritanceMain.java
@@ -0,0 +1,16 @@
+package com.tnsif.programs.daythree;
+
+public class SingleInheritanceMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student st=new Student("Ramya",342567459,"Mumbai",786532987,42,"saboo");
+		/*Setters are also used for setting the values of rollNo and collegeName
+		 * st.setRollNo(42);
+		 * st.setCollegeName("saboo");
+		 */
+		System.out.println(st);
+	}
+
+}
